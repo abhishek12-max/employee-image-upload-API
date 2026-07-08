b04955a789a97daf9734e5ddf561e9c8db8fa434
abhishek <ap7785980@gmail.com>
2026-07-08 13:43:43 +0530

    feat: model craeted sucesssfully

---
+++ src/model/employee.model.js
@@ -0,0 +1,51 @@
+const mongoose= require("mongoose");
+const employeeschema= new mongoose.Schema({
+    fullname:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        minlength:3,
+        maxlength:20
+    },
+    email:{
+        type:String,
+        rqeuired:true,
+        unique:true,
+        lowercase:true,
+        trim:true
+    },
+
+    password:{
+        type:String,
+        required:true,
+        minlength:6
+    },
+
+    role:{
+        type:String,
+        enum:["admin","manager","employee"],
+        default:"employee"
+    },
+
+    profileimage:{
+        type:String,
+        default: ""
+    }
+
+},
+{
+    timestamps:true
+},
+
+)
+
+const Employee= mongoose.model("employee",employeeschema);
+
+module.exports= Employee;
